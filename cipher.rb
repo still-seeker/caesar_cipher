a8b9981fe9c1138830d028f6c866d98eed0677db
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-03-06 04:17:08 +0200

    Caesar-cipher, substrings, stock_picker and bubble_sort code

---
+++ cipher.rb
@@ -0,0 +1,10 @@
+#This method shifts characters in a string by a given factor(in this case shift)
+def cipher(string, shift)
+  shifted_array = []
+
+  #Transform all characters to lowercase, get the bytes and map them
+  string.downcase.bytes.map do |c|
+    shifted_array.push ( c + shift < 123? (c + shift): (c + shift - 26))
+  end
+  shifted_array.pack('c*') #apply directive as many times as possible
+end
---
+++ substrings.rb
@@ -0,0 +1,27 @@
+#This method takes a string(one  or more words) and runs a check 
+#through a dictionary which is passed as an argument
+#and counts every appearance of all substrings in the dictionary
+#then finally returns a hash with substrings as keys and count of apperance as values
+def substrings(string, dictionary)
+  result = {}
+  string_case = string.downcase
+  
+  dictionary.each do |str|
+    str_lower = str.downcase
+    str_length = str_lower.length
+    max_index = string_case.length - str_length
+    count = 0
+
+    (0..max_index).each do |i|
+      if string_case[i, str_length] == str_lower
+        count += 1
+      end
+    end
+    result[str] = count unless count.zero?
+  end
+  result
+end
+
+dictionary = ["below", "down", "go", "going", "horn", "how", "howdy", "it",
+"i", "low", "own", "part", "patner", "sit"]
+substrings("Howdy patner, sit down! How's it going?", dictionary)
