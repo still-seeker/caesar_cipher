4139d020fbbe4428837b19f9eaf8fbecd95750dd
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-05-23 12:54:26 +0300

    feat: raise error with wrong argument number or type

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -3,6 +3,8 @@
 # This method shifts characters in a string by a given factor(in this case shift)
 def cipher(string, shift)
   result = ''
+  raise ArgumentError unless shift.is_a?(Integer)
+
   string.each_char do |char|
     char_num = char.ord
     rotation = if char_num.between?(65, 90) || char_num.between?(97, 122)
