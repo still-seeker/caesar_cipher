4adec7b52751ccb265acf8aeb76707733e6e99a9
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-05-23 12:55:50 +0300

    test negative shifts and argument erros

--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -14,4 +14,22 @@ describe '#cipher' do
     encrypted = cipher('what a string!', 50)
     expect(encrypted).to eq('ufyr y qrpgle!')
   end
+
+  it 'works with small negative shifts' do
+    encrypted = cipher('what a string!', -5)
+    expect(encrypted).to eq('rcvo v nomdib!')
+  end
+
+  it 'works with large negative shifts' do
+    encrypted = cipher('what a string!', -50)
+    expect(encrypted).to eq('yjcv c uvtkpi!')
+  end
+
+  it 'does not raise error with correct arguments' do
+    expect { cipher('what a string!', 10) }.not_to raise_error
+  end
+
+  it 'raises an argument error when passed one argument' do
+    expect { cipher('what a string!') }.to raise_error(ArgumentError)
+  end
 end
